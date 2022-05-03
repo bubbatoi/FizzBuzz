2408d9d9f6c3721c18d674a0216c722a937e5e5d
bubbatoi <103533914+bubbatoi@users.noreply.github.com>
2022-05-03 21:00:34 +0100

    fizzbuzz
    complete fizzbuz

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -1,3 +1,17 @@
 
-// More Information about this assisngment is available in the README.md file
+// More Information about this assisngment is av
+for (let i = 1; i<=100; i++) {
+    if( i % 3 === 0 && i % 5===0) {
+        output("FizzBuzz")}
+    else if (i % 3 ===0) {
+        output("Fizz")
+    }
 
+
+
+else if (i % 5 ===0) {
+    output("Buzz")
+}
+
+else (output(i))
+}
